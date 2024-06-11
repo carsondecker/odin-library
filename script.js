51613774f8ea5fbc3f71c35ef23a2ecc1de36bfb
carsondecker <carsondecker1205@gmail.com>
2024-06-11 17:00:11 -0500

    full js functionality, no styling

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const formTitle = document.querySelector("#title");
 const formAuthor = document.querySelector("#author");
 const formPages = document.querySelector("#pages");
 const formRead = document.querySelector("#read");
+const bookContainer = document.querySelector(".book-container");
 
 const myLibrary = [];
 
@@ -23,15 +24,61 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
     modal.close();
     form.reset();
+    updateLibrary();
     console.log(myLibrary)
 }
 
-//function updateLibrary() {}
+function updateLibrary() {
+    bookContainer.innerHTML = '';
+    myLibrary.forEach((book, index) => { createBookCard(book, index); })
+}
 
-function submitForm(event) {
-    event.preventDefault();
-    addBookToLibrary(new Book(formTitle.value, formAuthor.value, formPages.value, formRead.checked));
+function createBookCard(book, index) {
+    card = document.createElement('div');
+    card.classList.add("book-card");
+    card.id = index;
+
+    title = document.createElement('span');
+    title.classList.add('book-title');
+    title.textContent = book.title;
+    card.appendChild(title);
+
+    author = document.createElement('span');
+    author.classList.add('book-author');
+    author.textContent = book.author;
+    card.appendChild(author);
+
+    pages = document.createElement('span');
+    pages.classList.add('book-pages');
+    pages.textContent = book.pages;
+    card.appendChild(pages);
+
+    read = document.createElement('button');
+    read.classList.add('book-read');
+    book.read ? read.textContent = 'Read' : read.textContent = 'Not Read'
+    book.read ? read.classList.add('read') : read.classList.add('not-read')
+    read.addEventListener("click", (index) => {
+        book.read = !book.read
+        book.read ? read.textContent = 'Read' : read.textContent = 'Not Read'
+        read.classList.toggle('read');
+        read.classList.toggle('not-read');
+    })
+    card.appendChild(read);
+
+    remove = document.createElement('button');
+    remove.classList.add('book-remove');
+    remove.textContent = 'Remove';
+    remove.addEventListener("click", () => {
+        myLibrary.splice(index, 1);
+        updateLibrary();
+    })
+    card.appendChild(remove);
+    
+    bookContainer.appendChild(card);
 }
 
 newBookButton.addEventListener("click", () => { modal.showModal(); });
-form.addEventListener("submit", submitForm);
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    addBookToLibrary(new Book(formTitle.value, formAuthor.value, formPages.value, formRead.checked));
+});
